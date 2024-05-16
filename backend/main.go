1155f14486c2c8855f3651825fd1363bd83e6549
Goh Yong Jie <gohyongjie1112@gmail.com>
2024-05-16 15:46:37 +0800

    feat: add logger and cors support

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -11,13 +13,16 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"github.com/patrickmn/go-cache"
+	"github.com/rs/cors"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type App struct {
-	DB    *sql.DB
-	cache *cache.Cache
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	DB       *sql.DB
+	cache    *cache.Cache
 }
 
 type Pokemon struct {
@@ -51,9 +56,14 @@ type TypeInfo struct {
 
 func main() {
 
+	addr := flag.String("addr", ":8080", "HTTP network address")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Error loading .env file:", err)
+		errorLog.Println("Error loading .env file:", err)
 		return
 	}
 
@@ -61,7 +71,7 @@ func main() {
 	authToken := os.Getenv("TURSO_AUTH_TOKEN")
 
 	if dbURL == "" || authToken == "" {
-		fmt.Println("DATABASE_NAME and AUTH_TOKEN must be set in the .env file")
+		errorLog.Println("Missing required environment variables. Please check .env file for database URL and auth token.")
 		return
 	}
 
@@ -69,30 +79,62 @@ func main() {
 
 	db, err := sql.Open("libsql", url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+		errorLog.Output(2, fmt.Sprintf("failed to open db %s: %s", url, err))
 		os.Exit(1)
 	}
 	defer db.Close()
 
+	infoLog.Println("Successfully connected to database")
+
 	pokemonCache := cache.New(5*time.Minute, 10*time.Minute)
 
-	app := &App{DB: db, cache: pokemonCache}
+	app := &App{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		DB:       db,
+		cache:    pokemonCache,
+	}
 
 	router := httprouter.New()
 	router.GET("/pokemon", app.GetPokemonHandler)
-	http.ListenAndServe(":8080", router)
+
+	// TODO: change url dynamically based on environment
+	handler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}).Handler(router)
+
+	srv := &http.Server{
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      handler,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 func (app *App) GetPokemonHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	name := r.URL.Query().Get("name")
 	if name == "" {
-		http.Error(w, "Name is required", http.StatusBadRequest)
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
 	pokemon, err := app.FetchPokemon(app.DB, name)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		app.serverError(w, err)
+		return
+	}
+
+	if pokemon == nil {
+		app.notFound(w)
 		return
 	}
 
@@ -135,26 +177,88 @@ func (app *App) FetchPokemon(db *sql.DB, name string) (*Pokemon, error) {
 
 	app.cache.Set(name, &pokemon, cache.DefaultExpiration)
 
-	return &pokemon, nil
+	return pkm, nil
 }
 
 func fetchPokemonFromDatabase(db *sql.DB, name string) (*Pokemon, error) {
-	rows, err := db.Query("SELECT * FROM pokemon WHERE name = ?", name)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	if !rows.Next() {
-		return nil, nil
-	}
+	query := `
+		SELECT p.id, p.name, p.sprite_url 
+		FROM pokemon p 
+		WHERE p.name = ?
+	`
+	row := db.QueryRow(query, name)
 
 	var pokemon Pokemon
-	if err := rows.Scan(&pokemon.ID, &pokemon.Name); err != nil {
+	var spriteURL string
+
+	if err := row.Scan(&pokemon.ID, &pokemon.Name, &spriteURL); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	pokemon.Sprites.FrontDefault = spriteURL
+
+	if err := fetchPokemonAbilities(db, &pokemon); err != nil {
+		return nil, err
+	}
+
+	if err := fetchPokemonTypes(db, &pokemon); err != nil {
 		return nil, err
 	}
 
 	return &pokemon, nil
+}
+
+func fetchPokemonAbilities(db *sql.DB, pokemon *Pokemon) error {
+	query := `
+		SELECT pa.is_hidden, pa.slot, a.name, a.pokeapi_url
+		FROM pokemon_ability pa
+		JOIN ability a ON pa.ability_id = a.id
+		WHERE pa.pokemon_id = ?
+		ORDER BY pa.slot
+	`
+	rows, err := db.Query(query, pokemon.ID)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var ability Ability
+		if err := rows.Scan(&ability.IsHidden, &ability.Slot, &ability.Ability.Name, &ability.Ability.URL); err != nil {
+			return err
+		}
+		pokemon.Abilities = append(pokemon.Abilities, ability)
+	}
+
+	return rows.Err()
+}
+
+func fetchPokemonTypes(db *sql.DB, pokemon *Pokemon) error {
+	query := `
+		SELECT pt.slot, t.name, t.pokeapi_url
+		FROM pokemon_type pt
+		JOIN type t ON pt.type_id = t.id
+		WHERE pt.pokemon_id = ?
+		ORDER BY pt.slot
+	`
+	rows, err := db.Query(query, pokemon.ID)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var typeInfo TypeInfo
+		if err := rows.Scan(&typeInfo.Slot, &typeInfo.Type.Name, &typeInfo.Type.URL); err != nil {
+			return err
+		}
+		pokemon.Types = append(pokemon.Types, typeInfo)
+	}
+
+	return rows.Err()
 }
 
 func insertPokemonIntoDatabase(db *sql.DB, pokemon *Pokemon) error {
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/rs/cors v1.11.0 // indirect
 	github.com/tursodatabase/libsql-client-go v0.0.0-20240416075003-747366ff79c4 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	nhooyr.io/websocket v1.8.10 // indirect
